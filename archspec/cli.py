d003c1efe5e44da9adba54777a9d2c8327adc28b
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-05-11 09:31:08 +0200

    use subcommands for archspec CLI

--- archspec/cli.py
+++ archspec/cli.py
@@ -7,16 +7,21 @@ import sys
 
 
 def main():
-    parser = argparse.ArgumentParser(description="archspec command line interface", 
+    parser = argparse.ArgumentParser(description="archspec command line interface",
                                      prog='archspec')
+    parser.add_argument('-V', '--version', action='version', version=archspec.__version__)
 
-    parser.add_argument('--cpu-host', help="Print name of host CPU microarchitecture",
-                        action='store_true')
-    parser.add_argument('--version', action='version', version=archspec.__version__)
+    subparsers = parser.add_subparsers()
+
+    cpu_parser = subparsers.add_parser('cpu')
+
+    cpu_host_parser = cpu_parser.add_subparsers().add_parser('host')
+    cpu_host_parser.add_argument('cpu.host', help="print name of host CPU microarchitecture",
+                                 action='store_true')
 
     args = parser.parse_args()
 
-    if args.cpu_host:
+    if getattr(args, 'cpu.host'):
         print(archspec.cpu.host())
         sys.exit(0)
     else:
