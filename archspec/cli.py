37be3dca0caa315c15e9970f58e7417ae80addb4
Todd Gamblin <tgamblin@llnl.gov>
2020-06-17 14:50:37 -0700

    Archspec now has its own release number.
    Archspec grew out of Spack, so we initially just labeled it that way, but
    it's really its own project. I put in a separate code release for
    Archspec, so now we don't have to mention Spack in all the code headers.
    
    - [x] added the new release number, LLNL-CODE-811653, to `README.md`
    - [x] changed all the SPDX headers to only mention archspec
    - [x] changed copyright year to start of archspec

--- archspec/cli.py
+++ archspec/cli.py
@@ -1,6 +1,5 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
-# Spack and Archspec Project Developers. See the top-level COPYRIGHT file
-# for details.
+# Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
+# Archspec Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 """
