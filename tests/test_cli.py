85ba49b9531a667625aab3240d52dd9c29eaacd1
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-09-04 17:27:01 +0200

    fix Black code style issues in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -41,21 +41,25 @@ def test_cli_cpu_dag():
         #   power9 [label=power9]
         #   haswell [label=haswell]
         #   nocona [label="nocona (GenuineIntel)"]
-        assert re.search(r"^\s+" + uarch.name + r"\s+\[label=.+\]$", result.stdout, re.M)
+        assert re.search(
+            r"^\s+" + uarch.name + r"\s+\[label=.+\]$", result.stdout, re.M
+        )
         # every non-generic CPU microarchitecture has at least one parent
         # examples:
         #   haswell -> broadwell
         #   x86_64 -> nocona
         #   power7 -> power8
         if uarch.vendor != "generic":
-            assert re.search(r"^\s+[0-9a-z_]+\s+->\s+" + uarch.name + "$", result.stdout, re.M)
+            assert re.search(
+                r"^\s+[0-9a-z_]+\s+->\s+" + uarch.name + "$", result.stdout, re.M
+            )
 
     # hard check for a couple of patterns
     patterns = [
         "x86_64 [label=x86_64]",
         "power9 [label=power9]",
         "haswell [label=haswell]",
-        "nocona [label=\"nocona (GenuineIntel)\"]",
+        'nocona [label="nocona (GenuineIntel)"]',
         "x86_64 -> nocona",
         "power7 -> power8",
         "haswell -> broadwell",
