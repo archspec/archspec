f0b7e49626a45c2cb84c36df2f4bff58f9c28c5c
hugo <hugo@ip-0A001006.azs40guimkoujhvech5owurnud.ax.internal.cloudapp.net>
2021-10-15 08:03:59 +0000

    improving code style

--- archspec/interconnect/detect.py
+++ archspec/interconnect/detect.py
@@ -21,24 +21,22 @@ def host():
     if os.path.exists(sys_class_net_path): 
       network_devices = [f for f in os.listdir(sys_class_net_path)] 
       for device in network_devices:
-        if os.path.exists(sys_class_net_path+device+"/operstate"): 
-          f = open(sys_class_net_path+device+"/operstate",'r')
-          device_state = f.readline().strip()
+        dev_state_fp = os.path.join(sys_class_net_path, device, "operstate")
+        with open(dev_state_fp, 'r') as fp:
+          device_state = fp.readline().strip()
           if device_state == "up":
-            interconnect_devices.append("ethernet_"+device)
-          f.close()
+            interconnect_devices.append("ethernet_" + device)
 
     # check for infiniband capable devices
     sys_class_infiniband_path = "/sys/class/infiniband/"
     if os.path.exists(sys_class_infiniband_path): 
       infiniband_devices = [f for f in os.listdir(sys_class_infiniband_path)] 
       for device in infiniband_devices:
-        if os.path.exists(sys_class_infiniband_path+device+"/ports/1/state"): 
-          f = open(sys_class_infiniband_path+device+"/ports/1/state",'r')
-          device_state = f.readline().strip()
+        dev_state_fp = os.path.join(sys_class_infiniband_path, device, "ports", "1", "state")
+        with open(dev_state_fp, 'r') as fp:
+          device_state = fp.readline().strip()
           if device_state == "4: ACTIVE":
-            interconnect_devices.insert(0,"infiniband_"+device)
-          f.close()
+            interconnect_devices.insert(0, "infiniband_" + device)
 
     return (interconnect_devices[0])
 
