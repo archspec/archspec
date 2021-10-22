71480b310f098a157107b4b190861baa8e3fd5db
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-10-22 11:44:27 +0200

    Bump version number to v0.1.3 (#61)
    Also update the reference for git submodules

--- tests/test_archspec.py
+++ tests/test_archspec.py
@@ -6,6 +6,6 @@ import archspec
 
 
 def test_version():
-    assert archspec.__version__ == "0.1.2"
+    assert archspec.__version__ == "0.1.3"
     with open("pyproject.toml") as fp:
         assert 'version = "' + archspec.__version__ + '"\n' in fp.read()
