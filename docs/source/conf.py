ee08d0afe3aab97bbfd19c6b12362f968b5b9033
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-11-15 12:28:17 +0100

    Drop Python 2.7 and Python 3.5,3.6 support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Archspec's Contributors"
 author = "Archspec's Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
