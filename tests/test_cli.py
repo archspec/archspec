0f55474e484f8de1ef51c5fcdd808b6b60f6d655
Martin Larralde <martin.larralde@embl.de>
2023-01-17 22:11:20 +0100

    Rewrite `archspec` CLI with `argparse` instead of `click` (#109)
    This PR removes the `click` dependency for the CLI by using the built-in
    `argparse` module instead.
    
    I also added a `__main__.py` file so that the `archspec` CLI can be invoked
    as a module as well, with `python -m archspec`; I think this is quite standard
    for modules that also install an executable.
    
    * Use `argparse` instead of `click` to implement `archspec.cli`
    * Add `__main__.py` to allow invoking the CLI as a module
    * Remove `click` dependency from `pyproject.toml`
    * Remove unused class from `archspec.cli`
    * Remove `click` import from `archspec.cli`
    * Fix linter and `black` issues in `archspec.cli`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,21 +2,22 @@
 # Archspec Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+import io
+from unittest import mock
+
 import pytest
-import click.testing
 import archspec
 import archspec.cli
 
 
 @pytest.mark.parametrize("cli_args", [("--help",), ("cpu", "--help"), ("cpu",)])
 def test_command_run_without_failure(cli_args):
-    runner = click.testing.CliRunner()
-    result = runner.invoke(archspec.cli.main, cli_args)
-    assert result.exit_code == 0
+    result = archspec.cli.main(cli_args)
+    assert result == 0
 
 
 def test_cli_version():
-    runner = click.testing.CliRunner()
-    result = runner.invoke(archspec.cli.main, ("--version"))
-    assert result.exit_code == 0
-    assert result.stdout == "archspec, version " + archspec.__version__ + "\n"
+    with mock.patch("sys.stdout", new_callable=io.StringIO) as stdout:
+        result = archspec.cli.main(["--version"])
+    assert result == 0
+    assert stdout.getvalue() == "archspec, version " + archspec.__version__ + "\n"
