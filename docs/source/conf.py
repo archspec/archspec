48b92512b9ce203ded0ebd1ac41b42593e931f7c
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-05-04 15:37:50 +0200

    Bump version from v0.2.3 to v0.2.4 (#166)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ copyright = "2020, Archspec's Contributors"
 author = "Archspec's Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
