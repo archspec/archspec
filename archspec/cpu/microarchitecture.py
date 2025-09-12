275d2a5201a22ef8a74398e7055a87a5a55a57fe
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-09-12 20:45:22 +0200

    Add the concept of a `MicroarchitectureRange`

--- archspec/cpu/microarchitecture.py
+++ archspec/cpu/microarchitecture.py
@@ -8,7 +8,7 @@ import platform
 import re
 import sys
 import warnings
-from typing import IO, Any, Dict, List, Optional, Set, Tuple, Union
+from typing import IO, Any, Dict, FrozenSet, List, Optional, Set, Tuple, Union, Iterator
 
 from . import schema
 from .alias import FEATURE_ALIASES
@@ -233,6 +233,17 @@ class Microarchitecture:
             cpu_part=data.get("cpupart", ""),
         )
 
+    @staticmethod
+    def from_string(name: str) -> "Microarchitecture":
+        """Returns a micro-architecture from its name.
+
+        Raises:
+            ValueError: if the name is not a valid microarchitecture
+        """
+        if name not in TARGETS:
+            raise ValueError(f"unknown micro-architecture '{name}'")
+        return TARGETS[name]
+
     def optimization_flags(self, compiler: str, version: str) -> str:
         """Returns a string containing the optimization flags that needs to be used to produce
         code optimized for this micro-architecture.
@@ -327,6 +338,176 @@ class Microarchitecture:
             msg += " [no supported compiler versions]"
         msg = msg.format(self.name, compiler, version)
         raise UnsupportedMicroarchitecture(msg)
+
+
+def microarchitecture_min(m1: Microarchitecture, m2: Microarchitecture) -> Microarchitecture:
+    """Returns the most generic micro-architecture, if arguments are comparable
+
+    Raises:
+        ValueError: if arguments are not comparable
+    """
+    if m1 <= m2:
+        return m1
+
+    if m2 <= m1:
+        return m2
+
+    raise ValueError(f"{m1} and {m2} are not comparable")
+
+
+def microarchitecture_max(m1: Microarchitecture, m2: Microarchitecture) -> Microarchitecture:
+    """Returns the most specific micro-architecture, if arguments are comparable
+
+    Raises:
+        ValueError: if arguments are not comparable
+    """
+    if m1 <= m2:
+        return m2
+
+    if m2 <= m1:
+        return m1
+
+    raise ValueError(f"{m1} and {m2} are not comparable")
+
+
+class MicroarchitectureRange:
+    """A range of micro-architectures"""
+
+    def __init__(
+        self, *, lo: Optional[Microarchitecture] = None, hi: Optional[Microarchitecture] = None
+    ) -> None:
+        """Represents a range of microarchitectures, defined by a lower and an upper boundary.
+
+        Both boundaries are optional but must maintain logical consistency when defined.
+
+        If a boundary is None, the range is unbounded in that direction.
+        The lower boundary can always be inferred from the upper boundary and corresponds to
+        the family of the upper boundary.
+
+        If both boundaries are None, the range is empty.
+
+        Intersection and union are supported between ranges only if they don't result in
+        ambiguous upper or lower boundaries.
+
+        Args:
+            lo: The lower boundary of the microarchitecture range.
+            hi: The upper boundary of the microarchitecture range.
+
+        Raises:
+            InvalidRange: If the provided range boundaries are not consistent
+        """
+        if lo is not None and hi is not None and not lo <= hi:
+            raise InvalidRange(
+                f"the range ({lo}, {hi}) is invalid, since '{lo}' is not compatible with '{hi}'"
+            )
+
+        self.lo = lo
+        self.hi = hi
+
+        self._data: FrozenSet[Microarchitecture] = frozenset()
+        if self.lo and self.hi:
+            self._data = frozenset(x for x in TARGETS.values() if lo <= x <= hi)
+        elif self.lo:
+            self._data = frozenset(x for x in TARGETS.values() if lo <= x)
+        elif self.hi:
+            self._data = frozenset(x for x in TARGETS.values() if x <= hi)
+            # lo can be inferred from hi, but not vice versa
+            self.lo = self.hi.family
+
+    def empty(self) -> bool:
+        """Returns True if the range is empty, False otherwise"""
+        # If lo is not inferred, this is the empty set
+        return self.lo is None
+
+    def __contains__(self, item: Union[str, Microarchitecture]) -> bool:
+        if isinstance(item, str):
+            item = Microarchitecture.from_string(item)
+        return item in self._data
+
+    def __iter__(self) -> Iterator[Microarchitecture]:
+        return iter(self._data)
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __eq__(self, other: "MicroarchitectureRange") -> bool:
+        if not isinstance(other, MicroarchitectureRange):
+            return NotImplemented
+        return self.lo == other.lo and self.hi == other.hi and self._data == other._data
+
+    def __hash__(self) -> int:
+        return hash(self._data)
+
+    def family(self) -> Optional["Microarchitecture"]:
+        if self.lo is None:
+            return None
+        return self.lo.family
+
+    def __and__(self, other: "MicroarchitectureRange") -> "MicroarchitectureRange":
+        if not isinstance(other, MicroarchitectureRange):
+            return NotImplemented
+
+        # The intersection with the empty set is empty as well.
+        if other.empty() or self.empty():
+            return MicroarchitectureRange(lo=None, hi=None)
+
+        if self.family() != other.family():
+            raise ValueError(
+                f"cannot intersect {self} and {other}, since they belong to different families"
+            )
+
+        # One of the two is the empty range. The intersection is empty too.
+        if self.empty() or other.empty():
+            return MicroarchitectureRange(lo=None, hi=None)
+
+        new_data = self._data.intersection(other._data)
+        if not new_data:
+            return MicroarchitectureRange(lo=None, hi=None)
+
+        new_lo = min(*new_data)
+        if not all(new_lo <= x for x in new_data):
+            raise ValueError(f"cannot intersect {self} and {other}")
+
+        if self.hi is None:
+            new_hi = other.hi
+        elif other.hi is None:
+            new_hi = self.hi
+        else:
+            new_hi = max(*new_data)
+            if not all(x <= new_hi for x in new_data):
+                raise ValueError(f"cannot intersect {self} and {other}")
+
+        return MicroarchitectureRange(lo=new_lo, hi=new_hi)
+
+    def __or__(self, other: "MicroarchitectureRange") -> "MicroarchitectureRange":
+        if not isinstance(other, MicroarchitectureRange):
+            return NotImplemented
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(lo={self.lo!r}, hi={self.hi!r})"
+
+    def __str__(self) -> str:
+        if self.empty():
+            return "{}"
+
+        if self.hi is None:
+            return f"{{{self.lo} and higher}}"
+
+        return f"{{{self.lo}:{self.hi}}}"
+
+
+def microarchitecture_range(
+    *, lo: Optional[str] = None, hi: Optional[str] = None
+) -> MicroarchitectureRange:
+    """Returns a microarchitecture range from its boundaries.
+
+    Raises:
+        ValueError: if the name is not a valid microarchitecture
+    """
+    lo_uarch = lo if lo is None else Microarchitecture.from_string(lo)
+    hi_uarch = hi if hi is None else Microarchitecture.from_string(hi)
+    return MicroarchitectureRange(lo=lo_uarch, hi=hi_uarch)
 
 
 def generic_microarchitecture(name: str) -> Microarchitecture:
@@ -425,3 +606,7 @@ class UnsupportedMicroarchitecture(ArchspecError, ValueError):
 
 class InvalidCompilerVersion(ArchspecError, ValueError):
     """Raised when an invalid format is used for compiler versions in archspec."""
+
+
+class InvalidRange(ArchspecError, ValueError):
+    """Raised when an invalid range is constructed."""
