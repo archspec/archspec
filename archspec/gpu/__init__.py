3c973311cb8a51f433f43a1edf7cc29652c69a01
Aiden McCormack <89662634+Aiden2244@users.noreply.github.com>
2026-07-08 12:26:21 -0400

    Update archspec/gpu/__init__.py
    Co-authored-by: Tim Haines <thaines.astro@gmail.com>

--- archspec/gpu/__init__.py
+++ archspec/gpu/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2019-2026 Lawrence Livermore National Security, LLC and other
 # Archspec Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
